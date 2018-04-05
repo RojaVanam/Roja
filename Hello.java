9de9c81a4ec36111e0448e3dcc92a4e92ec58616
RojaVanam <37826026+RojaVanam@users.noreply.github.com>
2018-04-05 12:42:52 +0530

    Hello.java

--- Hello.java
+++ Hello.java
@@ -4,6 +4,11 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	System.out.println("Hello Word");
+		int a,b,c;
+		a=5;
+		b=10;
+		c=a+b;
+		System.out.println("Total c=" + c);
 		
 
 	}
